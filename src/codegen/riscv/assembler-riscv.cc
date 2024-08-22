367305f51e97bfc0b25184c4da6133a659b1b7ac
starjara <starjara@pusan.ac.kr>
2024-08-22 22:14:12 +0900

    No need the verse_read

--- src/codegen/riscv/assembler-riscv.cc
+++ src/codegen/riscv/assembler-riscv.cc
@@ -1594,9 +1594,13 @@ void Assembler::set_target_value_at(Address pc, uint64_t target,
   //if(*p2 != 0x15c637 && *p2 != 0x1ff537 && *p2 != 0x1ff637 && *p2 != 0xa2637 && *p2 != 0x68537 && *p2 != 0x1fa537 && *p2 != 0x2c637) {
   //if(*p2 == 0x155637 || *p2 == 0x155337 || *p2 == 0x155537 || *p2 == 0x157337 || *p2 == 0x1559b7 || *p2 == 0x1555b7) {
   if (0x155 <= imm && imm <= 0x157) {
-    //verse_read(pc, &p, sizeof(uint32_t));
+    /*
     p = verse_read((void *)pc, sizeof(uint32_t));
-    //printf("From Guest 0x%lx\n", p);
+    printf("From Guest 0x%lx\n", p);
+    */
+
+    p = *(p2);
+    // printf("From Host 0x%lx\n", p);
   }
   
   DCHECK_EQ((target & 0xffff000000000000ll), 0);
@@ -1618,65 +1622,74 @@ void Assembler::set_target_value_at(Address pc, uint64_t target,
   int64_t low_12 = high_31 & 0xfff;               // 12 bits
 
   if(p != 0) {
+    // printf("Write in guest pages\n");
     //*p = *p & 0xfff;
-    p = p & 0xfff;
+    p = *(p2) & 0xfff;
     verse_write((void *)pc, &p, sizeof(p));
     
     //*p = *p | ((int32_t)high_20 << 12);
-    p = p | ((int32_t)high_20 << 12);
+    p = *(p2) | ((int32_t)high_20 << 12);
     verse_write((void *)pc, &p, sizeof(p));
     
     //*(p + 1) = *(p + 1) & 0xfffff;
     //verse_read(pc + 4, &p, sizeof(p));
-    p = verse_read((void *)(pc + 4), sizeof(p));
-    p = p & 0xfffff;
+    /* JARA: No need the verse read */
+    // p = verse_read((void *)(pc + 4), sizeof(p));
+    // p = p & 0xfffff;
+    /* JARA End */
+    p = *(p2 + 1) & 0xfffff;
     verse_write((void *)(pc + 4), &p, sizeof(p));
     
     //*(p + 1) = *(p + 1) | ((int32_t)low_12 << 20);
-    p = p | ((int32_t)low_12 << 20);
+    p = *(p2 + 1) | ((int32_t)low_12 << 20);
     verse_write((void *)(pc + 4), &p, sizeof(p));
 
     //*(p + 2) = *(p + 2) & 0xfffff;
     //verse_read(pc + 8, &p, sizeof(p));
-    p = verse_read((void *)(pc + 8), sizeof(p));
-    p = p & 0xfffff;
+
+    // p = verse_read((void *)(pc + 8), sizeof(p));
+    // p = p & 0xfffff;
+    p = *(p2 + 2) & 0xfffff;
     verse_write((void *)(pc + 8), &p, sizeof(p));
   
     //*(p + 2) = *(p + 2) | (11 << 20);
-    p = p | (11 << 20);
+    p = *(p2 + 2) | (11 << 20);
     verse_write((void *)(pc + 8), &p, sizeof(p));
 
     //*(p + 3) = *(p + 3) & 0xfffff;
-    //verse_read(pc + 12, &p, sizeof(p));
-    p = verse_read((void *)(pc + 12), sizeof(p));
-    p = p & 0xfffff;
+    // p = verse_read((void *)(pc + 12), sizeof(p));
+    // p = p & 0xfffff;
+    
+    p = *(p2 + 3) & 0xfffff;
     verse_write((void *)(pc + 12), &p, sizeof(p));
 
     //*(p + 3) = *(p + 3) | ((int32_t)b11 << 20);
-    p = p | ((int32_t)b11 << 20);
+    p = *(p2 + 3) | ((int32_t)b11 << 20);
     verse_write((void *)(pc + 12), &p, sizeof(p));
 
-    //*(p + 4) = *(p + 4) & 0xfffff;
-    //verse_read(pc + 16, &p, sizeof(p));
-    p = verse_read((void *)(pc + 16), sizeof(p));
-    p = p & 0xfffff;
+    // *(p + 4) = *(p + 4) & 0xfffff;
+    // p = verse_read((void *)(pc + 16), sizeof(p));
+    // p = p & 0xfffff;
+    p = *(p2 + 4) & 0xfffff;
     verse_write((void *)(pc + 16), &p, sizeof(p));
     
-    //*(p + 4) = *(p + 4) | (6 << 20);
-    p = p | (6 << 20);
+    // *(p + 4) = *(p + 4) | (6 << 20);
+    p = *(p2 + 4) | (6 << 20);
     verse_write((void *)(pc + 16), &p, sizeof(p));
 
-    //*(p + 5) = *(p + 5) & 0xfffff;
-    //verse_read(pc + 20, &p, sizeof(p));
-    p = verse_read((void *)(pc + 20), sizeof(p));
-    p = p & 0xfffff;
+    // *(p + 5) = *(p + 5) & 0xfffff;
+    // p = verse_read((void *)(pc + 20), sizeof(p));
+    // p = p & 0xfffff;
+
+    p = *(p2 + 5) & 0xfffff;
     verse_write((void *)(pc + 20), &p, sizeof(p));
 
     //*(p + 5) = *(p + 5) | ((int32_t)a6 << 20);
-    p = p | ((int32_t)a6 << 20);
+    p = *(p2 + 5) | ((int32_t)a6 << 20);
     verse_write((void *)(pc + 20), &p, sizeof(p));
   }
   else {
+    // printf("Write in host pages\n");
     *p2 = *p2 & 0xfff;
     *p2 = *p2 | ((int32_t)high_20 << 12);
     *(p2 + 1) = *(p2 + 1) & 0xfffff;
